a70f62d0426f4e7c867d6e20a1e7e8ddc0cf3c39
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-03 22:14:27 +1000

    Added model for future use.

---
+++ DarkSky-dotnet/Models/Weather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DarkSky_dotnet.Models
+{
+	public class Weather
+	{
+		public double lat { get; set; }
+		public double lon { get; set; }
+	}
+}
