a40ff0f6ba3a9c2225677c146a58a07934f058bd
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-03 22:15:01 +1000

    Added darkskyapi.cs for api communication.

---
+++ DarkSky-dotnet/Models/DarkSky.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace DarkSky_dotnet.Models
+{
+	public class DarkSkyApi
+	{
+		readonly string APIKey;
+		readonly Uri baseURL = new Uri("https://api.darksky.net/");
+
+		public DarkSkyApi (string key)
+		{
+			this.APIKey = key;
+		}
+
+
+		public async Task GetFromAPI(double lat, double lon)
+		{
+
+		}
+
+	}
+}
