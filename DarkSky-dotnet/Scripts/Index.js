72916dd4036ce8919889ec37c0cf09f5532e4ec7
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-03 23:23:09 +1000

    Added ajax request to GetWeather

--- DarkSky-dotnet/Scripts/Index.js
+++ DarkSky-dotnet/Scripts/Index.js
@@ -18,10 +18,28 @@ function CalcWeather() {
 	var startPos;
 	var geoSuccess = function (position) {
 		startPos = position;
-		console.log("Position Calculated.");
+		console.log("Position Calculated for location: " + position.coords.latitude + " | " + position.coords.longitude);
+		CallToServer(position.coords.latitude, position.coords.longitude);
 	}
 	var geoError = function (error) {
 		console.log('Error occurred. Error code: ' + error.code);
 	}
 	navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}
+
+function CallToServer(lat, lon) {
+	console.log(lat + " | " + lon);
+	$.ajax({
+		type: "POST",
+		url: "Home/WeatherPost",
+		data: '{"lat": ' + lat + ',' + '"lon": ' + lon + '}',
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function (response) {
+			$("#partialView").html(response);
+		},
+		failure: function (response) {
+			alert(response);
+		}
+	});
 }
