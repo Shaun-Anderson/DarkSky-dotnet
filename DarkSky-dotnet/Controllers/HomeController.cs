03c69e0108b461f041b1cd53ddf9762162680ccb
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-03 23:23:39 +1000

    Added declaration and setting API key.

--- DarkSky-dotnet/Controllers/HomeController.cs
+++ DarkSky-dotnet/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DarkSky_dotnet.Models;
+using System.Threading.Tasks;
 
 namespace DarkSky_dotnet.Controllers
 {
@@ -11,6 +13,13 @@ namespace DarkSky_dotnet.Controllers
 		public ActionResult Index()
 		{
 			return View();
+		}
+
+		public async Task<ActionResult> WeatherPost (double lat, double lon)
+		{
+			DarkSkyApi api = new DarkSkyApi("abe5243cd0a2445af3fc3f8581caa352");
+			await api.GetForcast(lat, lon);
+			return PartialView();
 		}
 
 	}
