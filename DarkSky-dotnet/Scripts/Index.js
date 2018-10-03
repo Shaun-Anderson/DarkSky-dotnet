eb4b92b69166d0be2ed0d68baabf8812cb78a3c9
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-03 22:14:03 +1000

    Added JS for index view with function for geolocation

---
+++ DarkSky-dotnet/Scripts/Index.js
@@ -0,0 +1,27 @@
+﻿// On load
+$(function () {
+	$("#calcButton").click(CalcWeather);
+});
+
+// Use browsers navigator 
+function CalcWeather() {
+
+	// check for Geolocation support
+	if (navigator.geolocation) {
+		console.log('Geolocation is supported!');
+	}
+	else {
+		console.log('Geolocation is not supported for this Browser/OS.');
+		return;
+	}
+
+	var startPos;
+	var geoSuccess = function (position) {
+		startPos = position;
+		console.log("Position Calculated.");
+	}
+	var geoError = function (error) {
+		console.log('Error occurred. Error code: ' + error.code);
+	}
+	navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}
