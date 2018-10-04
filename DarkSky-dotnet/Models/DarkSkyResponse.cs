f0f45b41ec081b3d1ac077c5a753d569bb92c365
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-04 13:37:46 +1000

    Added timezone.

--- DarkSky-dotnet/Models/DarkSkyResponse.cs
+++ DarkSky-dotnet/Models/DarkSkyResponse.cs
@@ -15,5 +15,7 @@ namespace DarkSky_dotnet.Models
 		[JsonProperty(PropertyName = "longitude")]
 		public double Longitude { get; set; }
 
+		[JsonProperty(PropertyName = "timezone")]
+		public double Timezone { get; set; }
 	}
 }
