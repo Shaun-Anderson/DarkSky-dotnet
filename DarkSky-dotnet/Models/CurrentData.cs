d23e8bbe49ebff1d438d16b407c01ad4e7d55391
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-04 13:28:17 +1000

    Basic information added.

--- DarkSky-dotnet/Models/CurrentData.cs
+++ DarkSky-dotnet/Models/CurrentData.cs
@@ -4,8 +4,23 @@ namespace DarkSky_dotnet.Models
 {
 	public class CurrentData
 	{
+		private double Celcius;
+
+		[JsonProperty(PropertyName = "time")]
+		public string Time;
+
+		[JsonProperty(PropertyName = "summary")]
+		public string Summary;
+
+		[JsonProperty(PropertyName = "icon")]
+		public string Icon;
+
 		// Returns degrees in Fahrenheit format.
 		[JsonProperty(PropertyName = "temperature")]
-		public double Temperature { get; set; }
+		public double Temperature {
+			get { return Celcius; }
+			set { Celcius = 5.0 / 9.0 * (value - 32); }
+		}
+
 	}
 }
