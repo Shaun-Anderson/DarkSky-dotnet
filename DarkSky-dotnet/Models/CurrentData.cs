5e4f6b348a8985b7c32e8b890b313b25e0d0f014
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-04 11:01:21 +1000

    Added classes for JSON wrapping.

---
+++ DarkSky-dotnet/Models/CurrentData.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace DarkSky_dotnet.Models
+{
+	public class CurrentData
+	{
+		// Returns degrees in Fahrenheit format.
+		[JsonProperty(PropertyName = "temperature")]
+		public double Temperature { get; set; }
+	}
+}
---
+++ DarkSky-dotnet/Models/DarkSkyResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DarkSky_dotnet.Models
+{
+	public class DarkSkyResponse
+	{
+		[JsonProperty(PropertyName = "currently")]
+		public CurrentData Currently { get; set; }
+
+		[JsonProperty(PropertyName = "latitude")]
+		public double Latitude { get; set; }
+
+		[JsonProperty(PropertyName = "longitude")]
+		public double Longitude { get; set; }
+
+	}
+}
