643781628b061ab93bc4533b6dafc7848eb3c620
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-05 00:34:22 +1000

    Icon enum added.

---
+++ DarkSky-dotnet/Models/Icon.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace DarkSky_dotnet.Models
+{
+	
+	[JsonConverter(typeof(DarkSkyEnumJsonConverter))]
+	public enum Icon
+	{
+		[EnumMember(Value = null)]
+		None,
+
+		[EnumMember(Value = "clear-day")]
+		CLEAR_DAY,
+
+		[EnumMember(Value = "clear-night")]
+		CLEAR_NIGHT,
+
+		[EnumMember(Value = "rain")]
+		RAIN,
+
+		[EnumMember(Value = "snow")]
+		SNOW,
+
+		[EnumMember(Value = "sleet")]
+		SLEET,
+
+		[EnumMember(Value = "wind")]
+		WIND,
+
+		[EnumMember(Value = "fog")]
+		FOG,
+
+		[EnumMember(Value = "cloudy")]
+		CLOUDY,
+
+		[EnumMember(Value = "partly-cloudy-day")]
+		PARTLY_CLOUDY_DAY,
+
+		[EnumMember(Value = "partly-cloudy-night")]
+		PARTLY_CLOUDY_NIGHT,
+	}
+}
