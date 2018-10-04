1daf04f27245434ba0ca0b69d9cde98a998385bb
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-04 15:48:56 +1000

    Now includes all data sets.

---
+++ DarkSky-dotnet/Models/DataBlock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DarkSky_dotnet.Models
+{
+	public class DataBlock
+	{
+		[JsonProperty(PropertyName = "data")]
+		public List<Data> Data { get; set; }
+
+		[JsonProperty(PropertyName = "icon")]
+		public string Icon { get; set; }
+
+		[JsonProperty(PropertyName = "summary")]
+		public string Summary { get; set; }
+	}
+}
