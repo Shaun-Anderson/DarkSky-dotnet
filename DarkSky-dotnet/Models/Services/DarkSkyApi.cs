c24818b3d671264c0422eebc22f4a1b8666c15d0
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-05 12:46:18 +1000

    Removed unneeded using statements.

--- DarkSky-dotnet/Models/Services/DarkSkyApi.cs
+++ DarkSky-dotnet/Models/Services/DarkSkyApi.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Web.Mvc;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using DarkSky_dotnet.Models;
 
 namespace DarkSky_dotnet.Services
 {
