c79e963072dd709e00385359bedc994a2939ab66
Shaun Anderson <shaun.anderson@outlook.com>
2018-10-05 11:38:44 +1000

    Created WeatherViewModel to handle the data from both google and dark sky

---
+++ DarkSky-dotnet/Models/WeatherViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DarkSky_dotnet.Models
+{
+	
+	public class WeatherViewModel
+	{
+		public DarkSkyResponse darkSky;
+		public string address;
+	}
+}
